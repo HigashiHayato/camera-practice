13cd1b4ef40977c0e5f05a4c28bdba30e271faf9
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-05-28 12:44:13 +0900

    webcam 導入

---
+++ camera-app/src/components/Article.jsx
@@ -0,0 +1,11 @@
+import {Title} from "./index";
+
+const Article = (props) => {
+    return (
+        <div>
+            <Title title = {props.title} />
+        </div>
+    )
+}
+
+export default Article;
