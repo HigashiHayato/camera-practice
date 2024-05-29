aa98f7a97f7776bdcde0a50590fe0c61811c2940
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-05-29 14:24:42 +0900

    入力画面作成

--- camera-app/src/screen/Home.jsx
+++ camera-app/src/screen/Home.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
     return (
         <>
             <h1>aaaa</h1>
             <h1>aaaa</h1>
-            <Link to={"/input"}>入力画面</Link>
+            <Button variant="secondary" href="/input">入力画面</Button>
         </>
     )
 }
--- camera-app/src/screen/Input.jsx
+++ camera-app/src/screen/Input.jsx
@@ -4,9 +4,12 @@ const Input = () => {
     return (
         <>
             <h1>入力画面</h1>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Name" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
             <Link to={"/photo"}>写真撮影</Link>
         </>
     )
-}
+};
 
 export default Input;
