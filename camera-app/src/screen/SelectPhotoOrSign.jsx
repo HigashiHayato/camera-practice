eae5973c32863402985c8a7b135acc575d3c6a59
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-06-05 16:35:00 +0900

    選択するボタンを画面中央に配置

--- camera-app/src/screen/SelectPhotoOrSign.jsx
+++ camera-app/src/screen/SelectPhotoOrSign.jsx
@@ -1,28 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate, useLocation } from "react-router-dom";
+import './SelectPhotoOrSign.css'; // スタイルシートのインポート
 
 const SelectPhotoOrSign = () => {
-
     const location = useLocation();
     const text = location.state.text;
-    const textOthers = location.state.textOthers
+    const textOthers = location.state.textOthers;
     const navigate = useNavigate();
 
     return (
-        <div>
-            <Button
-                variant="secondary"
-                onClick={() => navigate('/photo', { state: { text, textOthers } })}>
-                写真撮影
-            </Button>
-            <Button
-                variant="secondary"
-                onClick={() => navigate('/digitalsign', { state: { text, textOthers } })}>
-                デジタルサイン
-            </Button>
+        <div className="fullscreen-container">
+            <div className="centered-content">
+                <Button
+                    variant="secondary"
+                    onClick={() => navigate('/photo', { state: { text, textOthers } })}
+                    className="button-margin">
+                    写真撮影
+                </Button>
+                <Button
+                    variant="secondary"
+                    onClick={() => navigate('/digitalsign', { state: { text, textOthers } })}
+                    className="button-margin">
+                    デジタルサイン
+                </Button>
+            </div>
         </div>
-
     )
 }
 
